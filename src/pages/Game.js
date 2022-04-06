a31ec6b69bbc555a51407f565af163c848924d4c
Gabriel Pesch <gkpesch@gmail.com>
2022-04-06 19:23:44 -0300

    requisito 05 e 06 concluido sem dificuldades SQN

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { fetchToken } from '../actions/index';
 import Header from '../components/Header';
 import { getQuestions } from '../helpers/api';
@@ -10,6 +10,7 @@ class Game extends React.Component {
     super();
     this.state = ({
       gameQuestions: [],
+      currentQuestion: 0,
     });
   }
 
@@ -26,33 +27,119 @@ class Game extends React.Component {
        this.handleQuestions();
        return;
      }
-     console.log(fetchQuestions);
+
      this.setState({
        gameQuestions: fetchQuestions.results,
      });
    }
 
+   randomizeQuestions = (arrayToRandomize) => {
+     const randomizeIndex = 0.5;
+     return arrayToRandomize.sort(() => Math.random() - randomizeIndex);
+   }
+
+   //  render() {
+   //    const { gameQuestions } = this.state;
+   //    return (
+   //      <>
+   //        <Header />
+   //        {
+   //          gameQuestions.map((question, index) => {
+   //            const allAnswers = [...question.incorrect_answers, question.correct_answer];
+   //            const randomAnswers = this.randomizeQuestions(allAnswers);
+   //            return (
+   //              <div key={ `question${index}` }>
+   //                <div>
+   //                  <h2 data-testid="question-category">{question.category}</h2>
+   //                  <p data-testid="question-text">{question.question}</p>
+   //                </div>
+
+   //                <div data-testid="answer-options">
+
+   // {randomAnswers.map((answer, answerIndex) => (
+   //   answer === question.correct_answer
+   //     ? (
+   //       <button
+   //         type="button"
+   //         data-testid="correct-answer"
+   //         key={ `answer${answerIndex}` }
+   //       >
+   //         {question.correct_answer}
+
+   //       </button>)
+   //     : (
+   //       <button
+   //         key={ `incorrect${answerIndex}` }
+   //         type="button"
+   //         data-testid={ `wrong-answer-${answerIndex}` }
+   //       >
+   //         {answer}
+
+   //       </button>)))}
+   //                </div>
+   //              </div>);
+   //          })
+   //        }
+   //      </>
+   //    );
+   //  }
    render() {
-     const { gameQuestions } = this.state;
-     console.log(gameQuestions);
+     const { gameQuestions, currentQuestion } = this.state;
      return (
        <>
-        <Header />
-         {gameQuestions
-           ? (
-             <>
-               <p>asdqaw</p>
-               <p>resposta1</p>
-               <p>resposta2</p>
-             </>
-           )
-           : null }
-         <p>teste</p>
-       </>
+         <Header />
+         {
 
+           gameQuestions.length > 0
+             ? (
+               <>
+                 <div>
+                   <h2 data-testid="question-category">
+                     {gameQuestions[currentQuestion].category}
+
+                   </h2>
+                   <p data-testid="question-text">
+                     {gameQuestions[currentQuestion].question}
+
+                   </p>
+                 </div>
+
+                 <div data-testid="answer-options">
+                   { this.randomizeQuestions(
+                     [...gameQuestions[currentQuestion].incorrect_answers,
+                       gameQuestions[currentQuestion].correct_answer],
+                   )
+                     .map((answer, answerIndex) => (
+                       answer === gameQuestions[currentQuestion].correct_answer
+                         ? (
+                           <button
+                             type="button"
+                             data-testid="correct-answer"
+                             key={ `answer${answerIndex}` }
+                           >
+                             {gameQuestions[currentQuestion].correct_answer}
+
+                           </button>)
+                         : (
+                           <button
+                             key={ `incorrect${answerIndex}` }
+                             type="button"
+                             data-testid={ `wrong-answer-${answerIndex}` }
+                           >
+                             {answer}
+
+                           </button>)))}
+                 </div>
+
+               </>
+             )
+             : null
+         }
+       </>
      );
    }
 }
+
 const mapDispatchToProps = (dispatch) => ({
   getPlayerToken: () => dispatch(fetchToken()),
 });
@@ -61,6 +148,9 @@ const mapStateToProps = (state) => ({
   token: state.token,
 });
 
-// Game.propTypes = {
-// };
+Game.propTypes = {
+  token: PropTypes.string.isRequired,
+  getPlayerToken: PropTypes.func.isRequired,
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
